9b753d4703c59b7e3d5b85327df0d161a88150d6
ManuNunez <manu.nunez.2004@gmail.com>
2023-08-23 15:47:16 -0600

    type scope :description

--- menu.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('menuBtn');
-const menuItems = document.getElementById('menuItems');
-
-menuBtn.addEventListener('click', () => {
-    menuItems.style.display = menuItems.style.display === 'none' ? 'block' : 'none';
-});
